387475f14e26b601618b516aec8216fe78c70d7d
ninaniet <n.nietyksza@gmail.com>
2025-11-04 13:26:09 +0100

    Add files via upload

---
+++ funkcja_kwadratowa.py
@@ -0,0 +1,39 @@
+import math
+
+n=1
+
+biblioteka = []
+
+delta = -b^2 * 4 * a * c
+
+for i in range(n):
+    print("podaj wartość a, b i c (cyfry wpisuj oddzielone spacją) ")
+    dane = input()
+    dane_split = dane.split()
+    a = int(dane_split[0])
+    b = int(dane_split[1])
+    c = int(dane_split[2])
+    biblioteka.append((a, b, c))
+
+delta = -b^2 * 4 * a * c
+
+ELIF!!!!!
+
+
+
+if a != 0:
+    delta > 0
+else:
+    b != 0
+
+    if delta > 0:
+        print("2")
+
+    else:
+        delta=0
+
+        if delta = 0:
+            print("1")
+
+        else:
+            print("0")
