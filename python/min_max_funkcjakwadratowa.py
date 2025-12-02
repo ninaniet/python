6cee2fb27707363d44ffd5b0d0d4e423b17fbe9b
ninaniet <n.nietyksza@gmail.com>
2025-12-02 13:26:04 +0100

    Add files via upload

---
+++ python/min_max_funkcjakwadratowa.py
@@ -0,0 +1,26 @@
+import math
+n = 1
+
+biblioteka = []
+
+for i in range(n):
+    dane = input()
+    dane_split = dane.split()
+
+    a = int(dane_split[0])
+    b = int(dane_split[1])
+    c = int(dane_split[2])
+    if 10>= a >= -10 and a!=0  
+        biblioteka.append((a, b, c))
+    
+for i in range(n):
+	dane = input()
+	dane_split = dane.split()
+
+	e = int(dane_split[0])
+	f = int(dane_split[1])
+    if 10>= e , f >= -10 and e < f
+	    biblioteka.append((e, f))
+    
+    
+
